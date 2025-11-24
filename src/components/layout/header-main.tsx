5b42465eeeefd532436b5f831abe135dcde3d05e
samuelndjouli997 <samuelndj.997@gmail.com>
2025-11-24 15:14:48 +0100

    💄 Feat: Add Deezer logo

--- src/components/layout/header-main.tsx
+++ src/components/layout/header-main.tsx
@@ -1,11 +1,14 @@
 import { HeaderSearchbar } from "@/components/layout/header-searchbar"
-import { HeaderLogo } from "@/components/layout/header-logo"
 import { ThemeToggle } from "@/components/theme-toggle"
 
 export const HeaderMain = () => {
   return (
-    <div className="min-h-16 px-6 flex justify-between items-center shadow-xs">
-      <HeaderLogo />
+    <div className="min-h-16 px-6 gap-x-4 flex justify-between items-center shadow-xs">
+      <img
+        src="/deezer-logo.svg"
+        className="content-[url(/deezer-mobile-logo.svg)] md:content-[url(/deezer-logo.svg)] md:dark:content-[url(/deezer-white-logo.svg)] size-full h-auto w-8 md:w-32"
+        alt="Deezer Logo"
+      />
       <HeaderSearchbar />
       <ThemeToggle />
     </div>
