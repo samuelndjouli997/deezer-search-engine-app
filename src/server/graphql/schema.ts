3dfb8f8e79cd0a703f9d6e147eddbcf18c95da90
samuelndjouli997 <samuelndj.997@gmail.com>
2025-12-05 20:18:29 +0100

    ✨ Feat: Create deezer schemas

--- src/server/graphql/schema.ts
+++ src/server/graphql/schema.ts
@@ -1,5 +1,30 @@
 export const typeDefs = `
-  type Query {
-    _health: String!
+  type Artist {
+    id: ID!
+    name: String!
+    biography: String
+    picture: String
   }
+
+  type Album {
+    title: String
+    cover: String
+  }
+
+  type Track {
+    id: ID!
+    title: String!
+    artist: Artist!
+    album: Album!
+    durant: Int
+    explicit_lyrics: Boolean
+    type: String!
+    preview: String
+    rank: Int
+  }
+
+    type Query {
+        _health: String! 
+        searchTrack(query: String!, limit: Int!, offset: Int!): [Track!]!
+    }
 `
