07d07c4618b7511cc7f391811ad9783b6c160533
samuel_ndjouli <samuelndj.997@gmail.com>
2025-12-30 00:53:19 +0100

    🐛 Fix: Fix artist biography

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -4,3 +4,20 @@ export const truncateStr = (text: string, maxLength: number = 30) => {
 
   return text.slice(0, maxLength) + "..."
 }
+
+export const discogsBiographyCodeToHTML = (biography: string) => {
+  return biography
+    .replace(/\[a=([^\]]+)\]/g, "<strong>$1</strong>")
+    .replace(
+      /\[a(\d+)\]/g,
+      '<a href="https://www.discogs.com/artist/$1" target="_blank" rel="noopener noreferrer" class="text-purple-600 underline font-semibold">view artist</a>'
+    )
+    .replace(/\[b=([^\]]+)\]/g, "<strong>$1</strong>")
+    .replace(/\[l=([^\]]+)\]/g, "<em>$1</em>")
+    .replace(
+      /\[url=([^\]]+)\]([^\[]+)\[\/url\]/g,
+      '<a href="$1" target="_blank" rel="noopener noreferrer" class="text-purple-600 underline font-semibold">$2</a>'
+    )
+    .replace(/\r\n\r\n/g, '</p><p class="mt-2">')
+    .replace(/\r\n/g, " ")
+}
